7c4de8c71bcaf423e54d91297cbd678550e7ee5f
Christian Gerhard <christian.gerhard@tui.com>
2024-04-05 13:27:30 +0200

    completed task 4

--- write.py
+++ write.py
@@ -29,11 +29,29 @@ def write_to_csv(results, filename):
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
     # TODO: Write the results to a CSV file, following the specification in the instructions.
+    if not results:
+        with open(filename, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            f.write('No data')
+    else:
+        with open(filename, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for approach in results:
+                writer.writerow({
+                    'datetime_utc': approach.time.strftime('%Y-%m-%d %H:%M'),
+                    'distance_au': approach.distance,
+                    'velocity_km_s': approach.velocity,
+                    'designation': approach.neo.designation,
+                    'name': approach.neo.name,
+                    'diameter_km': approach.neo.diameter,
+                    'potentially_hazardous': approach.neo.hazardous
+                })
 
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
-
     The precise output specification is in `README.md`. Roughly, the output is a
     list containing dictionaries, each mapping `CloseApproach` attributes to
     their values and the 'neo' key mapping to a dictionary of the associated
@@ -43,3 +61,25 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    output = []
+    if not results:
+        with open(filename, 'w') as f:
+            json.dump([], f)
+            f.write('\n')
+    else:
+        with open(filename, 'w') as f:
+            for approach in results:
+                output.append(
+                    {
+                        'datetime_utc': approach.time.strftime('%Y-%m-%d %H:%M'),
+                        'distance_au': approach.distance,
+                        'velocity_km_s': approach.velocity,
+                        'neo': {
+                            'designation': approach.neo.designation,
+                            'name': approach.neo.name if approach.neo.name else "",
+                            'diameter_km': approach.neo.diameter,
+                            'potentially_hazardous': approach.neo.hazardous if approach.neo.hazardous else False,
+                        },
+                    }
+                )
+            f.write(json.dumps(output))
